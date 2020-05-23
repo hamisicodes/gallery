ee1382d41bf13c8ffd1fa2342bef3ab9bfa86dba
hamisicodes <newhamisi@gmail.com>
2020-05-23 12:31:13 +0300

    create a model for an Image

--- personal_gallery/gallery/models.py
+++ personal_gallery/gallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    description  = models.TextField()
+    location = models.ForeignKey(Location , on_delete = models.CASCADE)
+    category = models.ForeignKey(Category , on_delete =models.CASCADE)
+    publisher = models.ForeignKey(User, on_delete = models.CASCADE)
+    image = models.ImageField(upload_to='images/')
