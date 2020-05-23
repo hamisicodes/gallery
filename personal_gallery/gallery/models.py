2647672213a287b38344e758120bf5a9802101e2
hamisicodes <newhamisi@gmail.com>
2020-05-23 12:34:23 +0300

    define a model for Location

--- personal_gallery/gallery/models.py
+++ personal_gallery/gallery/models.py
@@ -11,3 +11,7 @@ class Image(models.Model):
     category = models.ForeignKey(Category , on_delete =models.CASCADE)
     publisher = models.ForeignKey(User, on_delete = models.CASCADE)
     image = models.ImageField(upload_to='images/')
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
