c872d3f796add8f81105fb7ae84da1762674e402
hamisicodes <newhamisi@gmail.com>
2020-05-24 22:57:16 +0300

    define a view function that returns all images

--- personal_gallery/gallery/views.py
+++ personal_gallery/gallery/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Image
 
 # Create your views here.
+
+def image_list(request):
+    images  = Image.objects.all()
+
+    return render(request,'images.html' , {'images':images})
