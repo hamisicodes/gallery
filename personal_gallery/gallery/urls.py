645fa58b7d94d62b6fd50216d9112ca896bc3d93
hamisicodes <newhamisi@gmail.com>
2020-05-25 13:37:20 +0300

    refactor code in templates by template inheritance

--- personal_gallery/gallery/urls.py
+++ personal_gallery/gallery/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns =[
-    path('' , views.image_list , name = 'image_list')
+    path('' , views.image_list , name = 'image_list'),
+    path('category/<name>' , views.category , name = 'category')
 ]
 
 if settings.DEBUG:
--- personal_gallery/gallery/views.py
+++ personal_gallery/gallery/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Image
+from .models import Image,Category
 
 # Create your views here.
 
@@ -7,3 +7,9 @@ def image_list(request):
     images  = Image.objects.all()[:8]
 
     return render(request,'index.html' , {'images':images})
+
+def category(request,name):
+    category = Category.objects.get(name = name)
+    images = Image.objects.filter(category = category)
+
+    return render(request,'category.html' , {'images':images , 'category': category})
