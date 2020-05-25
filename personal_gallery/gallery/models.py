59f6d72ca60b0cbd86dd33e8d10e60e15dc05e8e
hamisicodes <newhamisi@gmail.com>
2020-05-25 10:57:11 +0300

    Add description field to the category model

--- personal_gallery/gallery/models.py
+++ personal_gallery/gallery/models.py
@@ -11,6 +11,7 @@ class Location(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
+    description =models.TextField()
 
     def __str__(self):
         return self.name
