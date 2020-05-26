19a606ba55b5594252c31f4f8b70a01b04bfdcdb
hamisicodes <newhamisi@gmail.com>
2020-05-26 22:53:44 +0300

    remove code from models

--- gallery/models.py
+++ gallery/models.py
@@ -41,14 +41,7 @@ class Category(models.Model):
 class Image(models.Model):
     name = models.CharField(max_length=30)
     description  = models.TextField()
-    location = models.ForeignKey(LDATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gallery',
-        'USER': 'dunchez',
-    'PASSWORD':'1955',
-    }
-}ocation , on_delete = models.CASCADE)
+    location = models.ForeignKey(Location , on_delete = models.CASCADE)
     category = models.ForeignKey(Category , on_delete =models.CASCADE)
     publisher = models.ForeignKey(User, on_delete = models.CASCADE)
     image = models.ImageField(upload_to='images/', default='hamisi')
